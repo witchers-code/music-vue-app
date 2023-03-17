8040757a4d613e7ae63f3b4471da1871beb8bfc8
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2023-03-17 20:49:25 +0100

    translation i18n

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -35,6 +35,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -49,6 +56,9 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "fr" ? "French" : "English";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -61,6 +71,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: "home" });
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "fr" ? "en" : "fr";
     },
   },
 };
